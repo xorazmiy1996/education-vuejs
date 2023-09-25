3d787d0747cf5eb606d23ccf0927b9e1a7eaed9e
@mibragimov <m.ibragimov@uzinfocom.uz>
2023-09-25 23:09:45 +0500

    update 25-netyabr 23:09

--- src/components/teacher/TeachersCourse.vue
+++ src/components/teacher/TeachersCourse.vue
@@ -12,13 +12,12 @@ export default {
 </script>
 
 <template>
-
   <div class="text-center mt-4">
     <h1 class="head-text">My courses</h1>
   </div>
   <div class="container">
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-      <my-course-card v-for="courses in user?.attend_courses" :courses="courses" :key="courses.id"/>
+      <my-course-card v-for="courses in user?.teach_courses" :courses="courses" :key="courses.id"/>
     </div>
   </div>
 
